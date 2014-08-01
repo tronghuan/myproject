afe91d3d5c1342921aea08635722858c353a7f03
tronghuan <huandt92@gmail.com>
2014-08-01 23:49:50 +0700

    Hoan chinh chuc nang them sua xoa

--- app/models/User.php
+++ app/models/User.php
@@ -1,13 +1,9 @@
 <?php
 
-use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
 
 	/**
 	 * The database table used by the model.
@@ -16,11 +12,65 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $table = 'users';
 
+	protected $guarded = array('id','password_confirmation');
+
+
+	public static $rules = array(
+		'name' => 'required|min:5',
+		'email' => 'required|email'
+	);
+
+
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password', 'remember_token');
+	protected $hidden = array('password');
+
+	/**
+	 * Get the unique identifier for the user.
+	 *
+	 * @return mixed
+	 */
+	public function getAuthIdentifier()
+	{
+		return $this->getKey();
+	}
+
+	/**
+	 * Get the password for the user.
+	 *
+	 * @return string
+	 */
+	public function getAuthPassword()
+	{
+		return $this->password;
+	}
+
+	/**
+	 * Get the e-mail address where password reminders are sent.
+	 *
+	 * @return string
+	 */
+	public function getReminderEmail()
+	{
+		return $this->email;
+	}
+	
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+	
+	public function setRememberToken($value)
+	{
+		$this->remember_token = $value;
+	}
+	
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -19,5 +19,5 @@ Route::get('/', function()
 Route::get('about', function() {
 	return View::make('about');
 });
-
-Route::resource('users', 'UserController');
+Route::get('contact', 'PagesController@contact');
+Route::resource('users', 'UsersController');
